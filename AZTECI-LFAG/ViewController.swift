47730414fc6be9e03e4375b848d3628ecca00043
Pipaldi <97627148+Pipaldi@users.noreply.github.com>
2022-01-17 14:09:12 -0600

    Download data function and display it in a Text View

--- AZTECI-LFAG/ViewController.swift
+++ AZTECI-LFAG/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // Press button to make API Call
+    @IBAction func downloadServices(_ sender: Any) {
+        
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        guard let url = URL(string: "http://74.208.173.11/asteci-servicios-core/api/v1/public/manager/app/info") else {
+            print("URL inválida")
+            return
+        }
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                return
+            }
+            guard let data = data else{
+                return
+            }
+            
+            if let result = String(data: data, encoding: .utf8){
+                print(result)
+                
+                DispatchQueue.main.async {
+                    self.textView.text = ("Data descargada: \n \(result)")
+                }
+                
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
 }
+
 
