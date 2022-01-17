da46d13cafdc6c371b31c95e45549b84ad0cf464
Pipaldi <97627148+Pipaldi@users.noreply.github.com>
2022-01-17 13:23:31 -0600

    Initial Commit

---
+++ AZTECI-LFAG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AZTECI-LFAG
+//
+//  Created by Luis Fernando AG on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AZTECI-LFAGTests/AZTECI_LFAGTests.swift
@@ -0,0 +1,33 @@
+//
+//  AZTECI_LFAGTests.swift
+//  AZTECI-LFAGTests
+//
+//  Created by Luis Fernando AG on 1/17/22.
+//
+
+import XCTest
+@testable import AZTECI_LFAG
+
+class AZTECI_LFAGTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
